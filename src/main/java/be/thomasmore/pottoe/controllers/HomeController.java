dad91b4a224036b131c9f4aafe342a5425b05f58
Floovera <r0385588@student.thomasmore.be>
2021-11-25 11:38:31 +0100

    waarde van het html element wordt vervangen door het th:text attribute

--- src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
+++ src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package be.thomasmore.pottoe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private String naam = "Floo";
+
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("naam",naam);
         return "home";
     }
 
