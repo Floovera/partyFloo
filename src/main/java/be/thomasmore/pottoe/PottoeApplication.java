7d229cebabc5994e38e640365329affa79810fb4
Floovera <r0385588@student.thomasmore.be>
2021-11-25 11:23:59 +0100

    add initial files

---
+++ src/main/java/be/thomasmore/pottoe/PottoeApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.pottoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PottoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PottoeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/pottoe/PottoeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.pottoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PottoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
