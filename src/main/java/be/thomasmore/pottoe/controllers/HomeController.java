1076efafbc81c1020a230936298088bee2856806
Floovera <r0385588@student.thomasmore.be>
2021-11-25 12:42:03 +0100

    collectielijst toegevoegd

--- src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
+++ src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
@@ -27,4 +27,9 @@ public class HomeController {
         return "collectie";
     }
 
+    @GetMapping("/collectielijst")
+    public String collectielijst(){
+        return "collectielijst";
+    }
+
 }
