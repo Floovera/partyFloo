3064c45ee6f539982a97a81cdd57d4581638003c
Floovera <r0385588@student.thomasmore.be>
2021-11-25 15:49:00 +0100

    array van strings verwijderd

--- src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
+++ src/main/java/be/thomasmore/pottoe/controllers/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class HomeController {
 
     private final String naam = "Floo";
-    private final String[] sets = {"Tasjes","Potjes","Bordjes"};
     private final Set[] setso= {
             new Set(0,"tasjes","Ilse"),
             new Set(1,"bordjes","Bea")
